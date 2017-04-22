7b8cc7ffc41c53c1ffcc34e1e45c963e76da9f48
Robin Liendeborg <robin@comichealth.se>
2017-04-22 13:26:06 +0200

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 angular.module(module.exports = 'app.tackpolisen.api', []);
 
-require('./services/checklist-service.js');
-require('./services/checklist-group-service.js');
 require('./services/checklist-item-service.js');
-
+require('./services/checklist-group-service.js');
+require('./services/instagram-service.js');
 require('./services/twitter-service.js');
 require('./services/facebook-service.js');
